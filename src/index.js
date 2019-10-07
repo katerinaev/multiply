9e18789dcb82f8cd783a920a10ed45d8e3d75a9b
katerinaev <katerina.ev@gmail.com>
2019-10-07 13:13:52 +0300

    solution multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let f = first.split('').reverse();
+  let s = second.split('').reverse();
+  let mult = [];
+  for (let i = 0; i < f.length; i++) {
+    for (let j = 0; j < s.length; j++) {
+      let m = f[i] * s[j];
+      mult[i + j] = (mult[i + j]) ? mult[i + j] + m : m;
+    }
+  } 
+  for (let k = 0; k < mult.length; k++) {
+    let n = mult[k] % 10;
+    let ten = Math.floor(mult[k] / 10);
+    mult[k] = n;
+    if (mult[k + 1]) {
+      mult[k+1] += ten;
+    } else if (ten != 0) {
+      mult[k + 1] = ten;
+    }
+  }
+    
+  return mult.reverse().join('');
 }
